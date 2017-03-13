7f4e88c48d53ca304c533a6723cacce9374c356c
Ernest Mkrtchyan <mkrtchyan.ernest@gmail.com>
2017-03-13 16:55:36 +0400

    Removed ignore files

--- index.js
+++ index.js
@@ -1,30 +1,24 @@
 "use strict";
 
-const express = require('express');
-const app = express();
-const server = require('http').createServer(app);
+const server = require('http').createServer();
 const io = require('socket.io')(server);
-
-app.get('/', function(req, res) {
-    res.send("BArev Sergo");
-});
 
 io.on('connection', function(client) {
 
-    console.log('Client connected...');
+    var address = client.handshake.address;
+    console.log('New connection from ' + address);
 
     client.on('join', function(data) {
         console.log(data);
         client.broadcast.emit("answer", data)
     });
 
-    client.on('event', function(data){});
     client.on('disconnect', function(){
 
         console.log("disconnected");
-
     });
 });
+
 server.listen(3000, function(){
 
     console.log('listening on *:3000');
